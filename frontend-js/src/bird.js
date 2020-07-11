62822f36dafd07aeb1a7b0e77c2706b85b6e2842
dannyflatiron <dannyflatiron@gmail.com>
2020-07-11 10:49:21 -0400

    getAllBirds gets all bird instances from API and adds all to DOM. Update createBirds to clear input field after submitting new data

--- frontend-js/src/bird.js
+++ frontend-js/src/bird.js
@@ -12,41 +12,44 @@ class Bird {
     }
 
     static createBird() {
-        // this caputes input from the user and preserves it in the database
-        // statis methods are not callable on instances of the class only on the class itself
+            // this caputes input from the user and preserves it in the database
+            // statis methods are not callable on instances of the class only on the class itself
 
-        // querySelector returns first instance 
-        // classSelector returns an array 
-        let birdForm = document.querySelector(".new-bird-form")
-        birdForm.addEventListener("submit", function(event) {
-            event.preventDefault()
-            
-                // captures form data
-    let formData = {
-        name: event.target[0].value,
-        species: event.target[1].value 
-    }
+            // querySelector returns first instance 
+            // classSelector returns an array 
+            let birdForm = document.querySelector(".new-bird-form")
+            birdForm.addEventListener("submit", function(event) {
+                event.preventDefault()
+                
+                    // captures form data
+        let formData = {
+            name: event.target[0].value,
+            species: event.target[1].value 
+        }
 
-    // configObj will tell fetch that this is a POST request and stringify data
-    let configObj = {
-        method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            // data exchanged between brwoser and server is sent as a text
-            // data assigned to the body of fetch request must be a string
-            body: JSON.stringify(formData)
-    }
+        // configObj will tell fetch that this is a POST request and stringify data
+        let configObj = {
+            method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                // data exchanged between brwoser and server is sent as a text
+                // data assigned to the body of fetch request must be a string
+                body: JSON.stringify(formData)
+        }
 
-    fetch("http://localhost:3000/birds", configObj)
-        .then(response => response.json()) 
-        .then(data => {
-            let newBird = new Bird(data)
-            newBird.addBirdToDom()
+        fetch("http://localhost:3000/birds", configObj)
+            .then(response => response.json()) 
+            .then(data => {
+                let newBird = new Bird(data)
+                newBird.addBirdToDom()
+            })
+            // clears input field 
+            event.target[0].value = ""
+            event.target[1].value = ""
         })
-        })
-    
+        
 
 
     }
@@ -69,7 +72,12 @@ class Bird {
         // add birds to DOM
         fetch("http://localhost:3000/birds")
         .then(response => response.json()) 
-        .then(data => console.log(data)) 
-
+        .then(data => {
+            // data is an array have to use map to access each element
+            data.map(c => {
+                let newBird = new Bird(c)
+                newBird.addBirdToDom() 
+            })
+        })
     }
 }
