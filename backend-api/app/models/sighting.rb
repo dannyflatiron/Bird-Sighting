7987fde78a31429b0ad9a099f2a5ef342ec421df
dannyflatiron <dannyflatiron@gmail.com>
2020-07-11 14:04:41 -0400

    added custom validation sighting_valid?

--- backend-api/app/models/sighting.rb
+++ backend-api/app/models/sighting.rb
@@ -1,5 +1,17 @@
 class Sighting < ApplicationRecord
   belongs_to :bird
   validates :date, presence: true
+  validate do
+    sighting_valid?
+  end
+  
+  
+  private 
+
+  def sighting_valid?
+    if self.date == false
+      self.errors.add(:invalid, "Please submit a date")
+    end
+  end
 
 end
