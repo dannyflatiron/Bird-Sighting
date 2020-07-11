9d1f5043dbc572c786d46c3fcf77e56b64343bb1
dannyflatiron <dannyflatiron@gmail.com>
2020-07-11 11:43:49 -0400

    added new sightings form

--- backend-api/app/controllers/sightings_controller.rb
+++ backend-api/app/controllers/sightings_controller.rb
@@ -7,6 +7,15 @@ class SightingsController < ApplicationController
         render json: { id: sighting.id, date: sighting.date, bird: sighting.bird }
     end
 
+    def create
+        sighting = Sighting.create(sightings_params)
+        if sighting.valid?
+            render json: sighting
+        else
+            render json: {error: "Sighting not valid"}
+        end
+    end
+
     private
 
     def sightings_params
