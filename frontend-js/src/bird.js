9a8f79cb943980aa790a823d1e9458e8d18b26f1
dannyflatiron <dannyflatiron@gmail.com>
2020-07-15 23:24:39 -0400

    troubled shooting best way to render sightings to the DOM. Global const main declared. Respositioned code for better readability

--- frontend-js/src/bird.js
+++ frontend-js/src/bird.js
@@ -1,5 +1,6 @@
+const main = document.querySelector("main")
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Bird.createBird()
     Bird.getAllBirds()
 })
 
@@ -9,106 +10,6 @@ class Bird {
         this.name = bird.name; 
         this.species = bird.species;
         this.sightings = bird.sightings;
-        // this.sightingDates = []
-    }
-    // static createBird() {
-    //         // this caputes input from the user and preserves it in the database
-    //         // statis methods are not callable on instances of the class only on the class itself
-
-    //         // querySelector returns first instance 
-    //         // classSelector returns an array 
-    //         let birdForm = document.querySelector(".new-bird-form")
-    //         birdForm.addEventListener("submit", function(event) {
-    //             event.preventDefault()
-                
-    //                 // captures form data
-    //     let formData = {
-    //         name: event.target[0].value,
-    //         species: event.target[1].value 
-    //     }
-
-    //     // configObj will tell fetch that this is a POST request and stringify data
-    //     let configObj = {
-    //         method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 "Accept": "application/json"
-    //             },
-    //             // data exchanged between brwoser and server is sent as a text
-    //             // data assigned to the body of fetch request must be a string
-    //             body: JSON.stringify(formData)
-    //     }
-
-    //     fetch("http://localhost:3000/birds", configObj)
-    //         .then(response => response.json()) 
-    //         .then(data => {
-    //             // console.log("data", data.error)
-    //             if (data.error) {
-    //                 const errorDiv = document.querySelector(".main")
-    //                 let pErrorMessage = document.createElement("p")
-    //                 pErrorMessage.setAttribute("class", "error-message")
-    //                 pErrorMessage.innerText = data.error
-    //                 errorDiv.appendChild(pErrorMessage)
-    //                 setTimeout(function(){
-    //                     document.querySelector(".error-message").innerText = ""
-    //                 }, 3000);
-    //             } else {
-    //                 let newBird = new Bird(data)
-    //                 newBird.addBirdToDom()
-    //             }
-    //         })
-    //         // clears input field 
-    //         event.target[0].value = ""
-    //         event.target[1].value = ""
-    //     })
-        
-
-
-    // }
-
-    // method to add a Bird instance from User submitted data to DOM
-    // certain HTML element (p tag) is being dynamically generated to hold Bird's name
-    // then p tag is being appended to appropriate div container
-    addBirdToDom() {
-        let main = document.querySelector("main")
-        let sightings = this.sightings
-        let dates = this.sightings.map(sighting => sighting.date)
-        // let pTagName = document.createElement("p")
-        // let pTagSpecies = document.createElement("p")
-        // pTagName.innerText = this.name
-        // pTagSpecies.innerText = this.species
-        // bird.appendChild(pTagName)
-        // bird.appendChild(pTagSpecies)
-        
-        const div = document.createElement("div")
-        const pName = document.createElement("p")
-        const pSpecies = document.createElement("p")
-        const pSightings = document.createElement("p")
-        const button = document.createElement("button")
-        const ul = document.createElement("ul")
-
-        div.setAttribute("class", "card")
-        div.setAttribute("data-id", this.id)
-        pName.innerText = `Name: ${this.name}`
-        pSpecies.innerText = `Species: ${this.species}`
-        pSightings.innerHTML 
-        if (dates.length === 0) {
-            pSightings.innerHTML = "Sightings Not Found"
-        } else {
-            pSightings.innerHTML = dates.join(', ')
-        }
-        button.setAttribute("data-bird-id", this.id)
-        button.innerText = "Add Sighting"
-        ul.setAttribute("class", "sightings")
-
-        div.appendChild(pName)
-        div.appendChild(pSpecies)
-        div.appendChild(pSightings)
-        div.appendChild(button)
-        div.appendChild(ul)
-        main.appendChild(div)
-
-        this.renderBirdSightigns(sightings)
     }
 
     static getAllBirds() {
@@ -120,29 +21,42 @@ class Bird {
             // data is an array have to use map to access each element
             data.forEach(bird => {
                 let newBird = new Bird(bird)
-                let sightings = newBird.sightings
-                let dates = sightings.map(sighting => sighting.date)
-                this.sightingDates = dates
                 newBird.addBirdToDom() 
             })
         })
     }
 
-    renderBirdSightigns(sightings) {
-        // let dates = sightings.map(sighting => sighting.date)
-        // console.log(dates)
+    // method to add a Bird instance from User submitted data to DOM
+    // certain HTML element (p tag) is being dynamically generated to hold Bird's name
+    // then p tag is being appended to appropriate div container
+    addBirdToDom() {
+        let dates = this.sightings.map(sighting => sighting.date)
+        const div = document.createElement("div")
+        const pName = document.createElement("p")
+        const pSpecies = document.createElement("p")
+        const button = document.createElement("button")
+        const ul = document.createElement("ul")        
 
-        // const ul = document.querySelector(".sightings")
-        // const li = document.createElement("li")
-        // const button = document.createElement("button")
+        div.setAttribute("class", "card")
+        div.setAttribute("data-id", this.id)
+        pName.innerText = `Name: ${this.name}`
+        pSpecies.innerText = `Species: ${this.species}`
+        button.setAttribute("data-bird-id", this.id)
+        button.innerText = "Add Sighting"
 
-        // li.innerHTML
-        // if (dates.length === 0) {
-        //     pSightings.innerHTML = "Sightings Not Found"
-        // } else {
-        //     pSightings.innerHTML = `Sightings: ${dates}`
-        // }
-        // button.setAttribute("button", "Remove Sighting")
-        // button.setAttribute("data-sighting-id")
+        div.appendChild(pName)
+        div.appendChild(pSpecies)
+        div.appendChild(button)
+        div.appendChild(ul)
+        main.appendChild(div)
+
+        if (this.sightings) {
+            this.sightings.forEach(function(sighting) {
+                let newSighting = new Sighting(sighting)
+                newSighting.renderBirdSightings()
+            })
+        }    
     }
+
+
 }
