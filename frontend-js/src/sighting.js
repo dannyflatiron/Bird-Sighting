41b7943fd91c5616c8c5606168a4d9e94a07aa44
dannyflatiron <dannyflatiron@gmail.com>
2020-07-15 23:26:19 -0400

    appended sightings to DOM with renderBirdSightings() called from addBirdToDom() in bird.js. Constructor is finished

--- frontend-js/src/sighting.js
+++ frontend-js/src/sighting.js
@@ -1,22 +1,24 @@
 class Sighting{
     constructor(sighting) {
+        this.id = sighting.id
         this.date = sighting.date
-        this.bird = sighting.bird
-        console.log(sighting);
+        this.bird_id = sighting.bird_id
+        // console.log(sighting)
     }
 
-    static getAllSightings() {
-        // get all birds from api
-        // add birds to DOM
-        fetch("http://localhost:3000/sightings")
-        .then(response => response.json()) 
-        .then(data => {
-            // data is an array have to use map to access each element
-            data.forEach(sighting => {
-                let newSighting = new Sighting(sighting)
-                // newBird.addBirdToDom() 
-            })
-        })
+    renderBirdSightings() {
+        console.log("this",this)
+        const ul = document.querySelector(`div[data-id="${this.bird_id}"`)
+        const li = document.createElement("li")
+        const button = document.createElement("button")
+        li.innerHTML = `Sightings: ${this.date}`
+
+        button.setAttribute("class", "remove")
+        button.setAttribute("data-sighting-id", this.id) 
+        button.innerHTML = "Remove"
+
+        li.appendChild(button)
+        ul.appendChild(li)
     }
    
 }
