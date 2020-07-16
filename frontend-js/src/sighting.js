e12a569b8bde0ed9c191ae344c236f0f3b74bc2b
dannyflatiron <dannyflatiron@gmail.com>
2020-07-16 04:47:34 -0400

    newSightingForm dynamically creates an html form, type=date. Issues appending it to DOM

--- frontend-js/src/sighting.js
+++ frontend-js/src/sighting.js
@@ -7,12 +7,12 @@ class Sighting{
     }
 
     renderBirdSightings() {
-        console.log("this",this)
         const ul = document.querySelector(`div[data-id="${this.bird_id}"`)
         const li = document.createElement("li")
         const button = document.createElement("button")
-        li.innerHTML = `Sightings: ${this.date}`
 
+        li.setAttribute("id", this.id)
+        li.innerHTML = `Sightings: ${this.date}`
         button.setAttribute("class", "remove")
         button.setAttribute("data-sighting-id", this.id) 
         button.innerHTML = "Remove"
@@ -20,5 +20,31 @@ class Sighting{
         li.appendChild(button)
         ul.appendChild(li)
     }
-   
+
+    static newSightingForm(bird) {
+        // console.log("sighting", bird_id)
+        let div = document.querySelector(".card")
+        let button = document.querySelector("data-bird-id")
+        const ul = document.querySelector(".sightingsList")
+
+
+        let form = document.createElement("form")
+        form.setAttribute("method", "post")
+        form.setAttribute("class", "sightingForm")
+
+        let inputDate = document.createElement("type", "date")
+
+
+        // let form = `<form id ="new-sighting-form>
+        // <label>What's your problem?:</label>
+        // <br><br><br><br><br>
+        // <label>What's your problem?:</label>
+        // <input type="date" value="2017-06-01">
+        // <input type="submit"/>
+        // </form>`
+
+        form.appendChild(inputDate)
+        ul.appendChild(form)
+        div.appendChild(ul)
+    }
 }
