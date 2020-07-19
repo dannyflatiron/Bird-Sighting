a8056a9c3622be0d7354994b1981029b6fe4bef4
dannyflatiron <dannyflatiron@gmail.com>
2020-07-19 02:47:44 -0400

    changed button to input to solve display issue. Append hiddentag to input instead of entire form. eventlistener prevents auto page reload and passes event and bird_id to createSighting function

--- frontend-js/src/sighting.js
+++ frontend-js/src/sighting.js
@@ -22,34 +22,97 @@ class Sighting{
     }
 
     static newSightingForm(bird, birdSightings) {
-        console.log("birdSightings", birdSightings)
-        console.log("bird", bird)
+        // console.log("birdSightings", birdSightings)
+        // console.log("bird", bird)
         const container = document.querySelector(".container")
         let div = document.querySelector(".card")
         const ul = document.getElementById(`${bird.id}`)
-        let button = document.querySelector("data-bird-id")
-        // how do I make sightingsList dynamic?
-        // class.sightingList#
-        // const ul = document.querySelector(".sightingsList")
 
         let birdId = bird.id
         let form = document.createElement("form")
         form.setAttribute("class", "sightingForm")
+        form.setAttribute("form-id", birdId)
+        // button.setAttribute("id", birdId)
         
 
         let inputDate = document.createElement("input")
+        inputDate.setAttribute("input-date-id", birdId)
         let hiddenTag = document.createElement("input")
         let submit = document.createElement("input")
         hiddenTag.type = "hidden"
         hiddenTag.value = `${birdId}`
         inputDate.type = "date"
         submit.type = "submit"
+        submit.setAttribute("data-submit-id", birdId)
 
+        inputDate.appendChild(hiddenTag)
         form.appendChild(inputDate)
-        form.appendChild(hiddenTag)
+        // form.appendChild(hiddenTag)
         form.appendChild(submit)
         ul.appendChild(form)
 
-        
+        submit.addEventListener("click", (event) => {
+            event.preventDefault()
+            // console.log("inputDate", inputDate)
+            // console.log("inputDate.value", inputDate.value)
+            // console.log("event.target", event.currentTarget.form[0].value)
+            Sighting.createSighting(event, birdId)
+            // console.log("birdid", birdId)
+            let date = inputDate.value
+        })
+
+        // Sighting.createSighting(form, birdId)
+
+    }
+
+    static createSighting(event, birdId) {
+        // console.log("event", event)
+        // console.log("birdId", birdId)
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    sighting: {
+                        date: event.currentTarget.form[0].value,
+                        bird_id: birdId
+                    }
+                }
+            )
+        }
+        fetch('http://localhost:3000/sightings', configObj)
+        .then(res => res.json())
+        .then(json => {
+            let newSighting = new Sighting(json)
+            newSighting.renderBirdSightings()
+        })
+        // let dateInput = document.getElementById("input-date-id").value
+        // let f = document.getElementById(`form[input-date-id=“${birdId}”]`)
+        // console.log("f", f)
+        // form.addEventListener('submit', function(event) {
+        //     event.preventDefault()
+        //     console.log("event", inputDate.value)
+        // })
+        // let form = document.getElementById("form-id")
+        // form.addEventListener("submit", function(event) {
+        //     event.preventDefault()
+        // }) 
+        // let sightingFormData = {
+        //     date: event.target[0].value
+        // }
+        // const configObj = {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application.json",
+        //         "Accept": "application/json"
+        //     },
+        //     body: JSON.stringify({
+        //         date: ,
+        //         bird_id: 
+        //     })
+        // }
     }
 }
