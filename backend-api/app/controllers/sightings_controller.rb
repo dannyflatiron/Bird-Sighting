72101b4a94d560849a9efa4ad3ffee49e060c3d9
dannyflatiron <dannyflatiron@gmail.com>
2020-07-19 02:49:14 -0400

    commented build method strategy and reinstated original create action code since bird id is being sent to the server via the fetch through the hidden id value in configObj

--- backend-api/app/controllers/sightings_controller.rb
+++ backend-api/app/controllers/sightings_controller.rb
@@ -12,16 +12,16 @@ class SightingsController < ApplicationController
     end
 
     def create
-        # sighting = Sighting.create(sightings_params)
-        # if sighting.save
-        #     render json: sighting
-        # else
-        #     render json: {message: sighting.errors.messages[:invalid]}
-        # end
-        bird = Bird.find_by(id: params[:bird_id])
-        bird.sightings.build(sightings_params)
-        sighting = bird.sightings.last
-        render json: bird.save ? bird : {message: sighting.errors.messages[:invalid]}
+        sighting = Sighting.create(sightings_params)
+        if sighting.save
+            render json: sighting
+        else
+            render json: {message: sighting.errors.messages[:invalid]}
+        end
+        # bird = Bird.find_by(id: params[:bird_id])
+        # bird.sightings.build(sightings_params)
+        # sighting = bird.sightings.last
+        # render json: bird.save ? bird : {message: sighting.errors.messages[:invalid]}
     end
 
     def destroy
