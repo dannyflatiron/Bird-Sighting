68ebd33fcb06d00dea2110ddc095a3f2f990ed03
dannyflatiron <dannyflatiron@gmail.com>
2020-07-20 11:51:50 -0400

    created searchBar feature. Debug needed to pass filteredBirds list to addBirdToDom function

--- frontend-js/index.js
+++ frontend-js/index.js
@@ -1,6 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    new Bird()
+document.addEventListener("DOMContentLoaded", init)
+let birdsArray = []
+const searchBar = document.getElementById('searchBar')
+
+function init() {
+    searchBirds()
+}
+
+function searchBirds() {
+
+    fetch("http://localhost:3000/birds")
+    .then(response => response.json()) 
+    .then(data => {
+        data.forEach(bird => {
+            let newBird = new Bird(bird)
+            birdsArray.push(newBird)
+        });
+    })
+
+    searchBar.addEventListener('keyup', (e) => {
+        const searchString = e.target.value.toLowerCase()
+        console.log("birdsArray", birdsArray)
+
+        const filteredBirds = birdsArray.filter((bird) => {
+            return (
+                bird.name.toLowerCase().includes(searchString) || bird.species.toLowerCase().includes(searchString)
+            )
+        })
+        console.log("filteredBirds", filteredBirds)
+        addBirdToDom(filteredBirds)
+    })
+}
 
 
-})
+
 
