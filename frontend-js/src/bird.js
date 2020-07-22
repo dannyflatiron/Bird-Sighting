67b266f60651f57501a1645f5b5bfabd2d053688
dannyflatiron <dannyflatiron@gmail.com>
2020-07-22 01:09:14 -0400

    debuggin search feature

--- frontend-js/src/bird.js
+++ frontend-js/src/bird.js
@@ -1,4 +1,6 @@
 const main = document.querySelector("main")
+const searchBar = document.getElementById('searchBar')
+let birdsArray = []
 
 document.addEventListener("DOMContentLoaded", function() {
     Bird.getAllBirds()
@@ -19,9 +21,53 @@ class Bird {
             data.forEach(bird => {
                 let newBird = new Bird(bird)
                 newBird.addBirdToDom() 
+
+                birdsArray.push(newBird)
             })
         })
+
+        searchBar.addEventListener('keyup', (e) => {
+            const searchString = e.target.value.toLowerCase()
+            console.log("birdsArray", birdsArray)
+            console.log("event", event)
+
+            const filteredBirds = birdsArray.filter((bird) => {
+                return (
+                    bird.name.toLowerCase().includes(searchString) || bird.species.toLowerCase().includes(searchString)
+                )
+            })
+            console.log("filteredBirds", filteredBirds)
+            // filteredBirds.forEach(bird => bird.addBirdToDom())
+            while (filteredBirds.length > 0) {
+                filteredBirds[0].addBirdToDom()
+            }
+        })
     }
+
+    // static searchBirds() {
+
+    //     fetch("http://localhost:3000/birds")
+    //     .then(response => response.json()) 
+    //     .then(data => {
+    //         data.forEach(bird => {
+    //             let newBird = new Bird(bird)
+    //             birdsArray.push(newBird)
+    //         });
+    //     })
+    
+    //     searchBar.addEventListener('keyup', (e) => {
+    //         const searchString = e.target.value.toLowerCase()
+    //         console.log("birdsArray", birdsArray)
+    
+    //         const filteredBirds = birdsArray.filter((bird) => {
+    //             return (
+    //                 bird.name.toLowerCase().includes(searchString) || bird.species.toLowerCase().includes(searchString)
+    //             )
+    //         })
+    //         console.log("filteredBirds", filteredBirds)
+    //         addBirdToDom(filteredBirds)
+    //     })
+    // }
 
     addBirdToDom() {
         let dates = this.sightings.map(sighting => sighting.date)
